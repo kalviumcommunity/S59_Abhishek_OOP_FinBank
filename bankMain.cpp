ccda092e19658425aba2814c03164364bc8b9c9a
abhishekkaundal0052 <kaundal.abhishek0052@gmail.com>
2024-08-21 11:33:17 +0530

    Added new and delete keyword

--- bankMain.cpp
+++ bankMain.cpp
@@ -1,24 +1,20 @@
 #include "bankingFunc.cpp"
 #include <iostream>
 
-int main() {
-    const int NUM_ACCOUNTS = 3;
+int main()
+{
 
-    Account accounts[NUM_ACCOUNTS] = {
-        Account(1001, 500.0),
-        Account(1002, 1000.0),
-        Account(1003, 1500.0)
-    };
+    Account *account = new Account(1001, 500.0);
     ATM atm;
 
-    for (int i = 0; i < NUM_ACCOUNTS; ++i) {
-        cout << "\nOperating on Account " << (i + 1) << " (Account Number: " << accounts[i].getAccountNumber() << ")\n";
+    atm.displayBalance(account);
+    
+    atm.deposit(account, 100.0);
 
-        atm.deposit(accounts[i], 100.0);
+    atm.withdraw(account, 50.0);
 
-        atm.withdraw(accounts[i], 50.0);
-
-        atm.displayBalance(accounts[i]);
-    }
+    atm.displayBalance(account);
+    delete account;
+    
     return 0;
 }
--- bankingFunc.cpp
+++ bankingFunc.cpp
@@ -19,25 +19,24 @@ int Account::getAccountNumber() const {
     return this->accountNumber;            
 }
 
-void ATM::deposit(Account &account, double amount) {
-    double newBalance = account.getBalance() + amount;
-    account.setBalance(newBalance);
+void ATM::deposit(Account *account, double amount) {
+    double newBalance = account->getBalance() + amount;
+    account->setBalance(newBalance);
     cout << "Deposit successful. New balance: " << newBalance << endl;
 }
 
-void ATM::withdraw(Account &account, double amount) {
-    double currentBalance = account.getBalance();
+void ATM::withdraw(Account *account, double amount) {
+    double currentBalance = account->getBalance();
     if (amount > currentBalance) {
         cout << "Insufficient funds." << endl;
     } else {
         double newBalance = currentBalance - amount;
-        account.setBalance(newBalance);
+        account->setBalance(newBalance);
         cout << "Withdrawal successful. Remaining balance: " << newBalance << endl;
     }
 }
 
-void ATM::displayBalance(const Account &account) const {
-    cout << "Account Number: " << account.getAccountNumber() << endl;
-    cout << "Current Balance: " << account.getBalance() << endl;
-    cout << "Balance displayed using ATM." << endl;
+void ATM::displayBalance(const Account *account) const {
+    cout << "Account Number: " << account->getAccountNumber() << endl;
+    cout << "Current Balance: " << account->getBalance() << endl;
 }
