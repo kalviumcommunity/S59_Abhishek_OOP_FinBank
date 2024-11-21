9f28430043a91c034ebc09c30f0deb8ccf69088b
abhishekkaundal0052 <kaundal.abhishek0052@gmail.com>
2024-11-21 11:23:03 +0530

    Applied Single Responsibility Principle

--- bankingFunc.cpp
+++ bankingFunc.cpp
@@ -17,60 +17,14 @@ int Account::getAccountNumber() const {
     return this->accountNumber;
 }
 
-int ATM::totalDeposits = 0;
-int ATM::totalWithdrawals = 0;
-
-int ATM::getTotalDeposits() {
-    return totalDeposits;
+void Account::displayAccountInfo() const {
+    cout << "Account Number: " << accountNumber << ", Current Balance: " << balance << endl;
 }
 
-void ATM::setTotalDeposits(int deposits) {
-    totalDeposits = deposits;
-}
-
-int ATM::getTotalWithdrawals() {
-    return totalWithdrawals;
-}
-
-void ATM::setTotalWithdrawals(int withdrawals) {
-    totalWithdrawals = withdrawals;
-}
-
-void ATM::deposit(Account* account, double amount) {
-    double newBalance = account->getBalance() + amount;
-    account->setBalance(newBalance);
-    setTotalDeposits(getTotalDeposits() + 1);
-    cout << "Deposit successful. New balance: " << newBalance << endl;
-}
-
-void ATM::withdraw(Account* account, double amount) {
-    double currentBalance = account->getBalance();
-    if (amount > currentBalance) {
-        cout << "Insufficient funds." << endl;
-    } else {
-        double newBalance = currentBalance - amount;
-        account->setBalance(newBalance);
-        setTotalWithdrawals(getTotalWithdrawals() + 1);
-        cout << "Withdrawal successful. Remaining balance: " << newBalance << endl;
-    }
-}
-
-void ATM::displayBalance(const Account* account) const {
-    account->displayAccountInfo();
-}
-
-void ATM::displayStats(int option) {
-    if (option == 1) {
-        cout << "Total Deposits: " << getTotalDeposits() << endl;
-    } else if (option == 2) {
-        cout << "Total Withdrawals: " << getTotalWithdrawals() << endl;
-    }
-}
-
-SavingsAccount::SavingsAccount(int accNum, double initialBalance, double rate) 
+SavingsAccount::SavingsAccount(int accNum, double initialBalance, double rate)
     : Account(accNum, initialBalance), interestRate(rate) {}
 
-SavingsAccount::SavingsAccount() 
+SavingsAccount::SavingsAccount()
     : Account(0, 0.0), interestRate(0.05) {}
 
 double SavingsAccount::calculateInterest() const {
@@ -87,14 +41,44 @@ void SavingsAccount::displayAccountInfo() const {
     cout << "Interest Rate: " << interestRate * 100 << "%" << endl;
 }
 
-BankBranch::BankBranch(int accNum, double initialBalance, const string& name) 
-    : Account(accNum, initialBalance), branchName(name) {}
-
-double BankBranch::calculateInterest() const {
-    return 0.0;
+void TransactionProcessor::deposit(Account* account, double amount) {
+    double newBalance = account->getBalance() + amount;
+    account->setBalance(newBalance);
+    StatisticsTracker::incrementDeposits();
+    cout << "Deposit successful. New balance: " << newBalance << endl;
 }
 
-void BankBranch::displayAccountInfo() const {
-    cout << "Branch: " << branchName << endl;
-    Account::displayAccountInfo();
+void TransactionProcessor::withdraw(Account* account, double amount) {
+    double currentBalance = account->getBalance();
+    if (amount > currentBalance) {
+        cout << "Insufficient funds." << endl;
+    } else {
+        double newBalance = currentBalance - amount;
+        account->setBalance(newBalance);
+        StatisticsTracker::incrementWithdrawals();
+        cout << "Withdrawal successful. Remaining balance: " << newBalance << endl;
+    }
+}
+
+void TransactionProcessor::displayBalance(const Account* account) const {
+    account->displayAccountInfo();
+}
+
+int StatisticsTracker::totalDeposits = 0;
+int StatisticsTracker::totalWithdrawals = 0;
+
+void StatisticsTracker::incrementDeposits() {
+    totalDeposits++;
+}
+
+void StatisticsTracker::incrementWithdrawals() {
+    totalWithdrawals++;
+}
+
+void StatisticsTracker::displayStats(int option) {
+    if (option == 1) {
+        cout << "Total Deposits: " << totalDeposits << endl;
+    } else if (option == 2) {
+        cout << "Total Withdrawals: " << totalWithdrawals << endl;
+    }
 }
