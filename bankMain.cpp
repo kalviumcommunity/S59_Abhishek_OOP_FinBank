b89d4ba885f8af2a2bd5b6f235775f8f06a0d9ad
abhishekkaundal0052 <kaundal.abhishek0052@gmail.com>
2024-12-06 20:44:26 +0530

    made some changes

--- bankMain.cpp
+++ bankMain.cpp
@@ -1,40 +1,30 @@
+#include "banking.h"
 #include "bankingFunc.cpp"
 #include <iostream>
 using namespace std;
 
 int main() {
-    SavingsAccount *savings = new SavingsAccount(1002, 2000.0, 0.05);
-    ATM atm;
+    SavingsAccount *savings = new SavingsAccount(1003, 10000.0, 0.05);
 
-    cout << "--- Savings Account ---"<<endl;
-    atm.displayBalance(savings);
+    TransactionProcessor atmProcessor;
 
-    atm.deposit(savings, 500.0);
-    atm.withdraw(savings, 200.0);
+    cout << "\n--- Savings Account ---\n";
+    atmProcessor.displayBalance(savings);
+
+    atmProcessor.deposit(savings, 500.0);
+    atmProcessor.withdraw(savings, 200.0);
 
     double interest = savings->calculateInterest();
     cout << "Calculated Interest: " << interest << endl;
-
-    atm.displayBalance(savings);
-
-    BankBranch *branch = new BankBranch(1003, 10000.0, "Main Branch");
-
-    cout << "--- Bank Branch ---"<<endl;
-    branch->displayBranchDetails();
-
-    atm.deposit(branch, 1500.0);
-    atm.withdraw(branch, 1200.0);
-
-    cout << "Updated Branch Details:"<<endl;
-    branch->displayBranchDetails();
+    savings->applyInterest();
+    atmProcessor.displayBalance(savings);
 
     int option;
-    cout << "Press '1' for checking Total Deposits and '2' for Total Withdrawals: ";
-    cin >> option; 
-    atm.displayStats(option); 
+    cout << "\nPress '1' for checking Total Deposits and '2' for Total Withdrawals: ";
+    cin >> option;
+    StatisticsTracker::displayStats(option);
 
     delete savings;
-    delete branch;
 
     return 0;
 }
--- banking.h
+++ banking.h
@@ -1,7 +1,8 @@
 #ifndef BANKING_H
 #define BANKING_H
 
-#include <string> 
+#include <iostream>
+#include <string>
 using namespace std;
 
 class Account {
@@ -14,24 +15,9 @@ public:
     double getBalance() const;
     void setBalance(double newBalance);
     int getAccountNumber() const;
-};
-
-class ATM {
-private:
-    static int totalDeposits;
-    static int totalWithdrawals;
-
-public:
-    static int getTotalDeposits();
-    static void setTotalDeposits(int deposits);
-
-    static int getTotalWithdrawals();
-    static void setTotalWithdrawals(int withdrawals);
-
-    void deposit(Account* account, double amount);
-    void withdraw(Account* account, double amount);
-    void displayBalance(const Account* account) const;
-    static void displayStats(int option);
+    virtual double calculateInterest() const = 0;
+    virtual void displayAccountInfo() const;
+    virtual ~Account() {}
 };
 
 class SavingsAccount : public Account {
@@ -40,16 +26,28 @@ private:
 
 public:
     SavingsAccount(int accNum, double initialBalance, double rate);
-    double calculateInterest() const;
+    SavingsAccount();
+    double calculateInterest() const override;
+    void applyInterest();
+    void displayAccountInfo() const override;
 };
 
-class BankBranch : public Account, public ATM {
+class TransactionProcessor {
+public:
+    void deposit(Account* account, double amount);
+    void withdraw(Account* account, double amount);
+    void displayBalance(const Account* account) const;
+};
+
+class StatisticsTracker {
 private:
-    string branchName;
+    static int totalDeposits;
+    static int totalWithdrawals;
 
 public:
-    BankBranch(int accNum, double initialBalance, const std::string& name);
-    void displayBranchDetails() const;
+    static void incrementDeposits();
+    static void incrementWithdrawals();
+    static void displayStats(int option);
 };
 
 #endif
